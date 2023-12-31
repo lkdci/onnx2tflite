e884da5401d7ac04c6cf3fa5880010ec5d91a780
danielafrimi <daniel.afrimi@mail.huji.ac.il>
2023-12-31 12:02:24 +0200

    add dataset for calibration

--- examples/yolonas_example.py
+++ examples/yolonas_example.py
@@ -1,15 +1,20 @@
-import onnx
-import torch
-from onnx2tflite.utils.builder import keras_builder
-import tensorflow as tf
+import hydra
 import numpy as np
+import onnx
+import super_gradients
+import tensorflow as tf
+import torch
+from omegaconf import DictConfig
+from super_gradients.training.datasets.detection_datasets import COCODetectionDataset
 
+from src.onnx2tflite.utils.builder import keras_builder
 
 class RandomDatasetGenerator:
-    def __init__(self, input_size, num_samples: int = 1):
+    def __init__(self, input_size, num_samples: int = 1, dataset=None):
         self.input_size = input_size
         self.counter = 0
         self.num_samples = num_samples
+        self.dataset = dataset
 
     def __iter__(self):
         self.counter = 0
@@ -20,18 +25,24 @@ class RandomDatasetGenerator:
 
     def __next__(self):
         self.counter += 1
-        if self.counter <= self.num_samples:
+        if self.counter <= self.num_samples and self.counter <= len(self.dataset):
+            if self.dataset:
+                sample = self.dataset[self.counter]
+                return [np.expand_dims(sample[0], axis=0)]
+            # Random input
             return [np.random.rand(*self.input_size).astype(np.float32)]
         raise StopIteration()
 
 
-if __name__ == '__main__':
+@hydra.main(config_path="../recipes/", config_name="coco2017_yolo_nas", version_base="1.2.0")
+def run(cfg: DictConfig):
+    cfg = hydra.utils.instantiate(cfg)
     input_size = [1, 3, 640, 640]
     channel_last_size = [input_size[0], input_size[2], input_size[3], input_size[1]]
     x_nchw = torch.randn(*input_size)
     quantize = True
 
-    onnx_path = "/Users/liork/Downloads/yolonas_s_for_tflite.onnx"
+    onnx_path = "/Users/danielafrimi/Downloads/yolox_model_no_heads.onnx"
     tflite_path = onnx_path.replace(".onnx", "_quant.tflite" if quantize else ".tflite")
 
     onnx_model = onnx.load_model(onnx_path)
@@ -40,10 +51,12 @@ if __name__ == '__main__':
     )
 
     converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
-    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS] #, tf.lite.OpsSet.SELECT_TF_OPS]
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]  # , tf.lite.OpsSet.SELECT_TF_OPS]
 
     if quantize:
-        converter.representative_dataset = RandomDatasetGenerator(input_size=channel_last_size, num_samples=5).iterator
+        dataset_val = COCODetectionDataset(**cfg.dataset_params.val_dataset_params)
+        converter.representative_dataset = RandomDatasetGenerator(input_size=channel_last_size, num_samples=5,
+                                                                  dataset=dataset_val).iterator
         converter._experimental_disable_per_channel = True
         converter.experimental_new_converter = True
         converter.experimental_new_quantizer = True
@@ -92,5 +105,6 @@ if __name__ == '__main__':
     # np.testing.assert_allclose(output_data, torch_out, rtol=1e-4)
 
 
-
-
+if __name__ == '__main__':
+    super_gradients.init_trainer()
+    run()
